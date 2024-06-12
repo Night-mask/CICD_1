443a57a4353a42beb6ff44ea316dcf8e3a89cc81
Night-mask <146056953+Night-mask@users.noreply.github.com>
2024-06-12 14:51:25 +0530

    Update calculator.py

--- CICD_1/calculator.py
+++ CICD_1/calculator.py
@@ -15,14 +15,14 @@ def calculator(a, b, operation):
     else:
         return 'Error: Invalid operation'
 
-if _name_ == "_main_":
-    if len(sys.argv) != 4:
-        print("Usage: python calculator.py <num1> <num2> <operation>")
-        sys.exit(1)
+# Direct execution of the script
+if len(sys.argv) != 4:
+    print("Usage: python calculator.py <num1> <num2> <operation>")
+    sys.exit(1)
 
-    num1 = float(sys.argv[1])
-    num2 = float(sys.argv[2])
-    operation = sys.argv[3]
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+operation = sys.argv[3]
 
-    result = calculator(num1, num2, operation)
-    print(f"Result: {result}")
+result = calculator(num1, num2, operation)
+print(f"Result: {result}")
